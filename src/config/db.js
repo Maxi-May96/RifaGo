420350f941a57fb8d180299240b0cd0cc8e3307b
Maxi-May96 <mayolstudio2024@gmail.com>
2026-07-13 12:26:37 -0300

    fix: throw error instead of process.exit in connectDB for serverless environment

--- src/config/db.js
+++ src/config/db.js
@@ -26,7 +26,7 @@ const connectDB = async () => {
     return conn;
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1);
+    throw error;
   }
 };
 
